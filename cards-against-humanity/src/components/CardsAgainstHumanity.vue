37b9a73c58bc8295ddaa68160b4773321d13b270
Derek Martin <derek.n.martin@gmail.com>
2018-03-12 21:32:15 -0400

    import json, chooseHand and card logic

---
+++ cards-against-humanity/src/components/CardsAgainstHumanity.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="main">
+    <h1>Cards Against Vue</h1>
+    <button @click="chooseHand">Deal Hand</button>
+    <button @click="chooseBlackCard">Deal Black Card</button>
+    <h3>{{ currentBlackCard.text }}</h3>
+    <template v-for="card in playerHand">
+      <p v-bind:key="card"> {{ card }} </p>
+    </template>
+  </div>
+</template>
+
+<script>
+import deck from '../assets/baseSet'
+
+export default {
+  name: 'CardsAgainstHumanity',
+  data() {
+    return {
+      blackCards: deck.blackCards,
+      whiteCards: deck.whiteCards,
+      playerHand: [],
+      currentBlackCard: '',
+    }
+  },
+  methods: {
+    randomCard(cardType) {
+      const randomNumber = Math.floor(Math.random() * Math.floor(cardType.length))
+      return cardType[randomNumber]
+    },
+    clearHand() {
+      this.playerHand = []
+    },
+    chooseHand() {
+      const numberOfCards = 10
+      this.clearHand()
+      for (let i = 0; i < numberOfCards; i += 1) {
+        this.playerHand.push(this.randomCard(this.whiteCards))
+      }
+    },
+    chooseBlackCard() {
+      this.currentBlackCard = this.randomCard(this.blackCards)
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
